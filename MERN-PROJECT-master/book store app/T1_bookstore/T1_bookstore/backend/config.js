27dda540cb578c7d865260daa61720425bf97396
sunkaribharath <sunkaribharathkumargoud@gmail.com>
2024-10-22 22:53:51 +0530

    Add files via upload

---
+++ MERN-PROJECT-master/book store app/T1_bookstore/T1_bookstore/backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL ="mongodb+srv://root:arjuncvinod@book-store.qoibc7b.mongodb.net/book-collection?retryWrites=true&w=majority";
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
